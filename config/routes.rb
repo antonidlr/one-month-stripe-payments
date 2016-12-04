2b1cbbf3228c1df4bd8e7c0b3ee8a73126b924e8
Antonio delosrios <antoniodelosrios22@gmail.com>
2016-12-04 14:39:19 -0300

    Implemented first cut of charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   
   root 'pages#home'
 
+  # Support stripe payments through charges
+  resources :charges
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
