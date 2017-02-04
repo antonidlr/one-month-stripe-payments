ce03cb6211e6d46a25fc9a3a893ae1acbeda0489
Antonio delosrios <antoniodelosrios22@gmail.com>
2017-02-04 18:52:54 -0300

    Added the ability to store purchases and display your purchase

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # Support stripe payments through charges
   resources :charges
 
+  # Route for showing purchases
+  resources :purchases, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
