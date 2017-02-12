acd6f686a8ef49931c2f249506d21c063adffcb0
Antonio delosrios <antoniodelosrios22@gmail.com>
2017-02-12 14:32:34 -0300

    Added products table and model

---
+++ db/migrate/20170212172754_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+    	t.string :title
+    	t.string :subtitle
+    	t.string :author
+    	t.text :description
+    	t.string :sku
+    	t.decimal :price
+      t.timestamps
+    end
+  end
+end
