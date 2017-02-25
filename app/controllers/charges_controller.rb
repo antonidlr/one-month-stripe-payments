98adf69862ebd6a50cb4254e725c13150b985797
Antonio delosrios <antoniodelosrios22@gmail.com>
2017-02-25 20:16:02 -0300

    Charging product price - not form field value

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,8 +9,8 @@ class ChargesController < ApplicationController
 
   charge = Stripe::Charge.create(
     :customer    => customer.id,
-    :amount      => params[:amount],
-    :description => 'Growth Hacking Crash Course',
+    :amount      => product.price_in_cents,
+    :description => product.full_description,
     :currency    => 'usd'
   )
 
